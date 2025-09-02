b767ccb52bb344dc30200f0f42e67c98743c032e
parham <parhamafshari666@gmail.com>
2025-09-02 18:09:37 +0330

    added the theming and initial chatbot ui

---
+++ components/chatbot/Chatbot.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export function Chatbot() {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <Card className="w-full h-[700px] flex flex-col">
+        {/* Chat Header */}
+        <CardHeader className="w-full">
+          <CardTitle>Chatbot</CardTitle>
+        </CardHeader>
+        
+        {/* Message Display Area */}
+        <CardContent className="w-full flex-1 space-y-4">
+          {/* Chat messages will go here */}
+        </CardContent>
+        
+        {/* Input and Send Area */}
+        <CardFooter className="w-full flex items-center space-x-2 p-4 border-t">
+          <Input placeholder="Type your message..." />
+          <Button className="cursor-pointer">Send</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
