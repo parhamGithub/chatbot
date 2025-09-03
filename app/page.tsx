7cc01b9052cdca1c4c32cc0bb38f7d90fcbba74b
Parham Afshari <parhamafshari666@gmail.com>
2025-09-03 09:18:25 +0330

    tested the api_key successfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,29 @@
-import { Chatbot } from "@/components/chatbot/Chatbot";
+import Chatbox from "@/components/chatbox/Chatbox";
+import { MessageCard } from "@/components/chatbox/MessageCard";
 import ModeToggle from "@/components/navbar/ModeToggle";
-// import { generateText } from "ai";
-// import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
 
-// export default async function Home() {
-//   const { text } = await generateText({
-//     model: google("models/gemini-2.0-flash-exp"),
-//     prompt: "What is love?",
-//   });
+export default async function Home() {
+  const { text } = await generateText({
+    model: google("models/gemini-2.0-flash-exp"),
+    prompt: "What is love?",
+  });
 
-export default function Navbar () {
   return (
-    <div className="w-full font-sans grid grid-rows-[20px_1fr_20px] items-center
-    justify-items-center min-h-screen gap-16 px-20 pt-5">
+    <div
+      className="w-full font-sans grid grid-rows-[20px_1fr_20px] items-center
+    justify-items-center min-h-screen gap-16 px-20 pt-5"
+    >
       <header className="w-full">
-        <nav className="w-full flex justify-end rounded-2xl p-4">
+        <nav className="w-full flex justify-end p-4">
           <ModeToggle />
         </nav>
       </header>
       <main className="w-full">
-        <Chatbot />
+        <Chatbox>
+          <MessageCard content={text} isUser={false} />
+        </Chatbox>
       </main>
     </div>
   );
