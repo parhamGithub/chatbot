c6c7b55d9ce9a573864f55c3acd3dcae84deab22
parham <parhamafshari666@gmail.com>
2025-09-16 10:51:55 +0330

    init

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,3 +1,32 @@
+// import { google } from "@ai-sdk/google";
+// import { streamText, UIMessage, convertToModelMessages } from "ai";
+// import { NextResponse } from "next/server";
+
+// // Allow streaming responses up to 30 seconds
+// export const maxDuration = 30;
+
+// export async function POST(req: Request) {
+//   try {
+//     const { messages }: { messages: UIMessage[] } = await req.json();
+
+//     const result = streamText({
+//       model: google("models/gemini-2.5-flash"),
+//       messages: convertToModelMessages(messages),
+//     });
+
+//     return result.toUIMessageStreamResponse();
+//   } catch (err) {
+//     console.error("Failed to stream text:", err);
+
+//     return NextResponse.json(
+//       {
+//         error: "An error occurred while processing your request.",
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 import { convertToModelMessages, streamText } from "ai";
 import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
 import { MODEL } from "@/generated/prisma";
@@ -43,7 +72,6 @@ export async function POST(req: Request) {
     messages: convertToModelMessages(messages),
     system: "you are nexiino, trained my nexiino co",
   });
-
 
   return result.toUIMessageStreamResponse();
 }
